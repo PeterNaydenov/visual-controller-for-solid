06e3495e61cb3dd7e96ea5a6eefa4724ca56226e
Peter Naydenov <peter.naydenov@gmail.com>
2026-04-19 19:14:30 +0300

    Cleaning...

--- demo/main.jsx
+++ demo/main.jsx
@@ -1,51 +1,51 @@
 import VisualController from '/src/main.js'
 import Hello from '/demo/hello.jsx'
 
-const html = new VisualController({})
+const 
+  html = new VisualController({})
+  , hasTextBlock = document.getElementById ( 'hasText' )
+  , updateMsgBtn = document.getElementById ( 'updateMsg' )
+  , incrementBtn = document.getElementById ( 'increment' )
+  , getCountBtn = document.getElementById ( 'getCount' )
+  , destroyBtn = document.getElementById ( 'destroy' )
+  , resultTextBlock = document.getElementById ( 'resultText' )
+  ;
 
 
 
 html.publish ( Hello, { greeting: 'Hi from Solid!' }, 'app')
     .then ( updates => {
-            console.log('App loaded with updates:', updates)
-            document.getElementById ( 'hasText' ).textContent = html.has('app')
+            console.log ( 'App loaded with updates:', updates )
+            hasTextBlock.textContent = html.has('app')
         })
 
 
 
-document
-  .getElementById ( 'updateMsg' )
-  .addEventListener ( 'click', () => {
-            const app = html.getApp('app')
+ updateMsgBtn.addEventListener ( 'click', () => {
+            const app = html.getApp ( 'app' )
             if (app)   app.changeMessage ( `Updated at ${new Date().toLocaleTimeString()}` )
       })
 
 
 
-document
-  .getElementById ( 'increment' )
-  .addEventListener ( 'click', () => {
+ incrementBtn.addEventListener ( 'click', () => {
             const app = html.getApp('app')
             if (app) app.increment()
       })
 
 
 
-document
-  .getElementById ( 'getCount' )
-  .addEventListener ( 'click', () => {
-            const app = html.getApp('app')
-            if (app) {
-              document.getElementById('resultText').textContent = app.getCount()
-            }
+getCountBtn.addEventListener ( 'click', () => {
+            const app = html.getApp ( 'app' )
+            if ( app ) {
+                resultTextBlock.textContent = app.getCount ()
+              }
       })
 
 
 
-document
-  .getElementById ( 'destroy' )
-  .addEventListener ( 'click', () => {
-            const result = html.destroy('app')
-            document.getElementById('resultText').textContent = 'Destroyed: ' + result
-            document.getElementById('hasText').textContent = html.has ( 'app' )
+destroyBtn.addEventListener ( 'click', () => {
+            const result = html.destroy ( 'app' )
+            resultTextBlock.textContent = 'Destroyed: ' + result
+            hasTextBlock.textContent = html.has ( 'app' )
       })
